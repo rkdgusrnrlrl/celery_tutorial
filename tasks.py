0d928882b5f01cba097de77197d2cd4b3341e674
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-03-15 21:08:05 +0900

    feature:change request example

--- tasks.py
+++ tasks.py
@@ -1,21 +1,35 @@
 from celery import Celery
-#import requests
+import requests
 
 app = Celery('tasks', broker='redis://localhost')
 
-#API_BASE = ''
+@app.task
+def check_api(api_monitor_request):
+    api_base_url = api_monitor_request.api_base_url
+    api_url = api_monitor_request.req_url
+    method = api_monitor_request.req_method
+    body = api_monitor_request.get_body()
+    headers = api_monitor_request.get_headers()
 
-@app.tasks
-def add(x, y):
-    """
-    body = {}
-    apiPath = ""
-    r = requests.post(API_BASE + apiPath, data=body)
+    r = call_api(api_base_url, api_url, body, headers, method)
+
+    if r is None:
+        return
 
     res_sec = r.elapsed.total_seconds()
-
     res_data = r.json()
     res_data['res_sec'] = res_sec
     return res_data
-    """
-    return x + y
+
+
+def call_api(api_base_url, api_url, body, headers, method):
+    if method is 'GET':
+        return requests.post(api_base_url + api_url, data=body, headers=headers)
+    elif method is 'POST':
+        return requests.post(api_base_url + api_url, data=body, headers=headers)
+    elif method is 'PUT':
+        return requests.post(api_base_url + api_url, data=body, headers=headers)
+    elif method is 'DELETE':
+        return requests.post(api_base_url + api_url, data=body, headers=headers)
+    else:
+        return None

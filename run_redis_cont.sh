98fd636e964b0888863b65456393fb8cd4655aa9
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-03-25 20:17:52 +0900

    feature:add remove redis container than run container

--- run_redis_cont.sh
+++ run_redis_cont.sh
@@ -1,4 +1,9 @@
 
 mkdir -p data
 
+if [[ "$(docker ps -a | grep "some-redis" 2> /dev/null)" != "" ]]; then
+  docker rm -f "some-redis"
+  echo "삭제 됨"
+fi
+
 docker run --name some-redis -d -v data:/data -p 6379:6379 redis redis-server --appendonly yes
